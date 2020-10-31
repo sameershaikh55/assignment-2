f5bec58433bba6be00e3fa1fab15d3a4cad600ad
sameer-shaikh <sameermubeen4@gmail.com>
2020-10-31 21:13:30 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ function App() {
   return (
     <div className="App">
       <h1>HELLO WORLD</h1>
+      <h1>Sameer_Shaikh (A Future Software Developer)</h1>
+      <h2>Thank You Sir Zia Team</h2>
     </div>
   );
 }
